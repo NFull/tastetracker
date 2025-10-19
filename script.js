e0065b50c33a03780b45d75e5338edb971f1dd47
NFull <natalie.fullerton530@gmail.com>
2025-10-18 21:13:00 -0400

    printFoodReccomendation function and test

--- script.js
+++ script.js
@@ -27,12 +27,18 @@ for (let i = 1; i <= favoriteFoods.length; i++) {
     // "Have you ever tried ____?"
     // "I always recommend ____ to friends."
     // "Trust me — ____ is delicious."
-
+function printFoodRecommendation(foodName) {
+  console.log("Have you tried " + foodName + "?");
+  console.log("I always recommend " + foodName + " to friends.");
+  console.log("Trust me - " + foodName + " is delicious.");
+};
 
 
 
 // 4b. Call the function at least 3 times
-
+console.log(printFoodRecommendation("Ramen"));
+console.log(printFoodRecommendation("Salsa"));
+console.log(printFoodRecommendation("Pho"));
 
 
 // Here's a list of 50 friends' favorite foods:
